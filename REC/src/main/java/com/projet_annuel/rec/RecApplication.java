f5ef8b8ffc1c595db7affeb4552b4d34f708e5b2
Ennasiri Mehdi <ennasirimehdi@gmail.com>
2022-05-30 11:09:33 +0200

    code-exec

---
+++ REC/src/main/java/com/projet_annuel/rec/RecApplication.java
@@ -0,0 +1,13 @@
+package com.projet_annuel.rec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecApplication.class, args);
+    }
+
+}
